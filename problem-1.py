b79826dd73e3e175d632342a19cb49b3fb2cc2c8
Sammers21 <titantins@ya.ru>
2017-02-28 00:09:06 +0300

    firts 2 problems solved

---
+++ problem-1.py
@@ -0,0 +1,57 @@
+from functools import reduce
+
+from scipy.stats import rv_continuous
+import numpy as np
+
+
+class customDist(rv_continuous):
+    """"
+    Distribution F(x)=1-exp(-exp(0.1x)
+    """
+
+    def _cdf(self, x, *args):
+        return 1 - np.exp(-np.exp(x / 10.))
+
+
+d = customDist(name='gaussian')
+x = d.rvs()
+
+list = []
+for i in range(100):
+    list.append(d.rvs())
+
+import matplotlib.pyplot as m
+
+f = m.figure()
+m.hist(list, bins=10)
+m.title("Distribution F(x)=1-exp(-exp(0.1x)) 100xValues")
+m.xlabel("Value")
+m.ylabel("Count of times")
+m.grid(True)
+
+list = []
+for i in range(1000):
+    list.append(d.rvs())
+
+f = m.figure()
+m.hist(list, bins=10)
+m.title("Distribution F(x)=1-exp(-exp(0.1x)) 1000xValues")
+m.xlabel("Value")
+m.ylabel("Count of times")
+m.grid(True)
+
+list = []
+for i in range(1000):
+    lst = []
+    for y in range(30):
+        lst.append(d.rvs())
+    list.append(reduce(lambda x, y: x + y, lst))
+
+f = m.figure()
+m.hist(list, bins=10)
+m.title("Distribution Yi=SUM j=1...30 Xij i=1...30 1000xValues")
+m.xlabel("Value")
+m.ylabel("Count of times")
+m.grid(True)
+
+m.show()
---
+++ problem-2.py
@@ -0,0 +1,80 @@
+from functools import reduce
+
+import numpy.random as l
+import numpy as n
+import matplotlib.pyplot as p
+
+mean = 3
+sigma = 1
+count_of_elements = 8
+# квантиль уровня 0.025 для N(0,1)
+cvantil0025gaussian = 2.81
+
+
+def test(m=mean, c=count_of_elements):
+    global h_one_true, h_two_true
+    # of course sum is 10000
+    h_one_true = 0
+    h_two_true = 0
+    for i in range(10000):
+        array_of_elements_from_distribution = l.normal(m, sigma, c)
+        average = reduce(lambda x, y: x + y, array_of_elements_from_distribution) / c
+        right = average + (cvantil0025gaussian * sigma / n.sqrt(c))
+        left = average - (cvantil0025gaussian * sigma / n.sqrt(c))
+        if (left < mean and mean < right):
+            h_one_true += 1
+        else:
+            h_two_true += 1
+    return (h_one_true, h_two_true)
+
+
+test()
+
+print("result is :\n"
+      "hypnosis than mean is 3 was true {0} times\n"
+      "hypnosis than mean isn't 3 was true {1} times\n"
+      "оценка вероятности ошибки первого рода {2} "
+      .format(h_one_true, h_two_true, h_two_true / 10000)
+      )
+"""
+result is :
+hypnosis than mean is 3 was true 9945 times
+hypnosis than mean isn't 3 was true 55 times
+оценка вероятности ошибки первого рода 0.0055
+"""
+
+counter = 1
+array_of_points = []
+array_of_counters = []
+for i in range(9):
+    test(counter)
+    array_of_points.append(h_two_true / 10000)
+    array_of_counters.append(counter)
+    counter += 0.5
+
+p.plot(array_of_counters, array_of_points,
+       linewidth=3,
+       color='r')
+
+counter = 1
+array_of_points = []
+array_of_counters = []
+for i in range(9):
+    test(counter, 50)
+    array_of_points.append(h_two_true / 10000)
+    array_of_counters.append(counter)
+    counter += 0.5
+
+p.plot(array_of_counters, array_of_points,
+       linewidth=3,
+       color='g')
+
+p.xlabel("Мат ожидание генерируемой выборки(настоящее)")
+p.ylabel("Вероятость отвергнуть основную гипотезу")
+
+p.text(2.3, 0.9, "Выборка из 50 элементов", rotation=300,color='g')
+p.text(1, 0.85, "Выборка из 8 элементов", rotation=300,color='r')
+
+
+
+p.show()
