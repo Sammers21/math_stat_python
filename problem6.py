be6f3565d918fd0e9cfd4ca52c7c7028400b0496
Pavel Drankov <titantins@gmail.com>
2017-06-04 15:43:37 +0300

    some things were deleted

--- problem6.py
+++ problem6.py
@@ -17,69 +17,6 @@ class_3 = read_column_from_csv(2, 'data/6problem_{}.csv'.format(v_number), type=
 sex = read_column_from_csv(3, 'data/6problem_{}.csv'.format(v_number), type='f')
 survived = read_column_from_csv(4, 'data/6problem_{}.csv'.format(v_number), type='f')
 
-# survived  overall count
-#       \   /
-c1_s_1 = [0, 0]
-c1_s_0 = [0, 0]
-c2_s_1 = [0, 0]
-c2_s_0 = [0, 0]
-c3_s_1 = [0, 0]
-c3_s_0 = [0, 0]
-
-# govno
-for i in range(len(class_1)):
-
-    if class_1[i] == 1:
-        if sex[i] == 1:
-            c1_s_1[1] += 1
-            c1_s_1[0] += survived[i]
-        else:
-            c1_s_0[1] += 1
-            c1_s_0[0] += survived[i]
-
-    elif class_2[i] == 1:
-        if sex[i] == 1:
-            c2_s_1[1] += 1
-            c2_s_1[0] += survived[i]
-        else:
-            c2_s_0[1] += 1
-            c2_s_0[0] += survived[i]
-
-    elif class_3[i] == 1:
-        if sex[i] == 1:
-            c3_s_1[1] += 1
-            c3_s_1[0] += survived[i]
-        else:
-            c3_s_0[1] += 1
-            c3_s_0[0] += survived[i]
-
-print(c1_s_1)
-print(c1_s_0)
-print(c2_s_1)
-print(c2_s_0)
-print(c3_s_1)
-print(c3_s_0)
-
-chances = []
-for i in range(len(class_1)):
-
-    if class_1[i] == 1:
-        if sex[i] == 1:
-            chances.append((c1_s_1[0] / c1_s_1[1]) / (1 - c1_s_1[0] / c1_s_1[1]))
-        else:
-            chances.append((c1_s_0[0] / c1_s_0[1]) / (1 - c1_s_0[0] / c1_s_0[1]))
-    elif class_2[i] == 1:
-        if sex[i] == 1:
-            chances.append((c2_s_1[0] / c2_s_1[1]) / (1 - c2_s_1[0] / c2_s_1[1]))
-        else:
-            chances.append((c2_s_0[0] / c2_s_0[1]) / (1 - c2_s_0[0] / c2_s_0[1]))
-
-    elif class_3[i] == 1:
-        if sex[i] == 1:
-            chances.append((c3_s_1[0] / c3_s_1[1]) / (1 - c3_s_1[0] / c3_s_1[1]))
-        else:
-            chances.append((c3_s_0[0] / c3_s_0[1]) / (1 - c3_s_0[0] / c3_s_0[1]))
-
 df = pd.DataFrame({
     "class_1": class_1,
     "class_2": class_2,
@@ -94,9 +31,6 @@ res = model.fit()
 print(res.summary())
 
 # Проверьте значимость модели в целом
-print(res.params[0])
-print(res.params[0])
-print(res.params[0])
 
 
 #ESS=ess(su)
