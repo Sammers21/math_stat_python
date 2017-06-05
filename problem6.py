34610d0d971f4d026723d67def276d86d5e88738
Pavel Drankov <titantins@gmail.com>
2017-06-05 22:56:49 +0300

    probit/logit

--- problem6.py
+++ problem6.py
@@ -2,31 +2,39 @@ import pandas as pd
 import statsmodels.formula.api as smf
 import math
 from lib import rss, ess
-from scipy.stats import f
+from scipy.stats import f, norm
+import numpy as np
 
 from lib import mk_data_var, read_column_from_csv
 
 # TODO alter this to your variant
-v_number = 1
+v_number = 10
 
 mk_data_var(v_number)
 
 class_1 = read_column_from_csv(0, 'data/6problem_{}.csv'.format(v_number), type='f')
 class_2 = read_column_from_csv(1, 'data/6problem_{}.csv'.format(v_number), type='f')
-class_3 = read_column_from_csv(2, 'data/6problem_{}.csv'.format(v_number), type='f')
 sex = read_column_from_csv(3, 'data/6problem_{}.csv'.format(v_number), type='f')
 survived = read_column_from_csv(4, 'data/6problem_{}.csv'.format(v_number), type='f')
 
 df = pd.DataFrame({
     "class_1": class_1,
     "class_2": class_2,
-    "class_3": class_3,
     "sex": sex,
     "survived": survived
 })
 
+###################################################################################################
+###############  Оцените модель логит для вероятности выжить в зависимости от #####################
+###############  пола, возраста и класса каюты                                #####################
+###############  Проверьте значимость модели в целом                          #####################
+###############  Рассчитайте прогнозные значения вероятностей выживания       #####################
+###############  1.1 LOGIT                                                    #####################
+###################################################################################################
+
+
 # Оцените модель логит для вероятности выжить в зависимости от пола, возраста и класса каюты:
-model = smf.logit(formula="survived ~ 1 + C(sex) + C(class_1) + C(class_2) + C(class_3)", data=df)
+model = smf.logit(formula="survived ~ sex + class_1 + class_2 ", data=df)
 res = model.fit()
 print(res.summary())
 
@@ -39,8 +47,7 @@ y_estimate = [1
                   res.params[0] +
                   res.params[1] * sex[i] +
                   res.params[2] * class_1[i] +
-                  res.params[3] * class_2[i] +
-                  res.params[4] * class_3[i]) > 1
+                  res.params[3] * class_2[i]) > 1
               else 0
 
               for i in range(len(sex))]
@@ -61,3 +68,48 @@ else:
 # В данном случае, показателем того, на сколько модель расходится с реальным положенимем дел
 # является RSS. В данном случаем значение RSS - колчество наблюдений, где модель расхоидтся с данными.
 print('Модель logit расходится с данными в {} случаях из {}'.format(rss_y, n))
+
+###################################################################################################
+###############  Оцените модель логит для вероятности выжить в зависимости от #####################
+###############  пола, возраста и класса каюты                                #####################
+###############  Проверьте значимость модели в целом                          #####################
+###############  Рассчитайте прогнозные значения вероятностей выживания       #####################
+###############  1.1 PROBIT                                                   #####################
+###################################################################################################
+
+
+# Оцените модель логит для вероятности выжить в зависимости от пола, возраста и класса каюты:
+model = smf.probit(formula="survived ~ 1 + sex + class_1 + class_2", data=df)
+res = model.fit()
+print(res.summary())
+
+# Проверьте значимость модели в целом
+
+# Прогноз выживания по модели
+y_estimate = [1
+              if
+              norm.cdf(
+                  res.params[0] +
+                  res.params[1] * sex[i] +
+                  res.params[2] * class_1[i] +
+                  res.params[3] * class_2[i]) > 0.5
+              else 0
+
+              for i in range(len(sex))]
+
+ess_y = ess(survived, y_estimate)
+rss_y = rss(survived, y_estimate)
+k = 4  # кол-во коэффициентов (с учётом свободного)
+n = len(survived)  # объём выборки
+
+f_crit = f.ppf(0.95, k - 1, n - k)
+f_real = ess_y / (k - 1) / (rss_y / (n - k))
+
+if f_crit < f_real:
+    print('Отвергаем гипотзу о значимости модели регрессии в целом H0:(b1=b2=b3=b4=0)')
+else:
+    print('Принмаем гипотзу о значимости модели регрессии в целом H0:(b1=b2=b3=b4=0)')
+
+# В данном случае, показателем того, на сколько модель расходится с реальным положенимем дел
+# является RSS. В данном случаем значение RSS - колчество наблюдений, где модель расхоидтся с данными.
+print('Модель probit расходится с данными в {} случаях из {}'.format(rss_y, n))
